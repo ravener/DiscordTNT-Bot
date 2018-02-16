720bb92a5b5e1ad9353e10cf68d5603aa9b3316e
freetnt5852 <31079629+freetnt5852@users.noreply.github.com>
2018-02-16 14:33:41 -0900

    Update exec.js

--- commands/exec.js
+++ commands/exec.js
@@ -1,6 +1,7 @@
 const exec = require('child_process').exec;
 
 exports.run = async(client, message, args) => {
+    if(message.author.id !== "292690616285134850") return client.sendMessage(message.channel_id, "This command is for owner only!");
     exec(`${args.join(' ')}`, (error, stdout) => {
       const response = (error || stdout);
       client.sendMessage(message.channel_id, `Ran: ${args.join(" ")}\n\`\`\`${response}\`\`\``)

1b82cab223ab41ca0114cbdb36ca6f68f655502f
freetnt5852 <31079629+freetnt5852@users.noreply.github.com>
2018-02-16 10:09:28 -0900

    Improve bot a lot

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const client = new DiscordTNT.Client({
     STATUS: "online"
 });
 
+const devs = [
+ "292690616285134850"
+];
+
 const prefix = "d.";
 
 client.on("ready", ready => {
@@ -13,9 +17,21 @@ client.on("ready", ready => {
 });
 
 client.on("messageCreate", message => {
-  if(!message.content.startsWith(prefix) || message.author.bot) return; // ignore bots and msgs don't begin with prefix
-  if(message.content.startsWith(prefix + "ping")) {
+  if(message.content.indexOf(prefix) !== 0) return;
+  if(message.author.bot) return; // ignore bots
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  if(command === "ping") {
    client.sendMessage(message.channel_id, "Pong!");
+  } else if(command === "eval") {
+      if(!devs.includes(message.author.id)) return client.sendMessage(message.channel_id, "This command is for devs only");
+      try {
+        let evaled = eval(args.join(" "));
+        client.sendMessage(message.channel_id, `\`\`\`js\n${evaled}\`\`\``);
+     } catch(e) {
+        client.sendMessage(message.channel_id, `\`\`\`${e}\`\`\``);
+     }
+   }
 });
 
 client.connect();

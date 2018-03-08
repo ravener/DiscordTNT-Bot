b259e6331719ac5810f5d72bfd01bd4515a90e73
freetnt5852 <31079629+freetnt5852@users.noreply.github.com>
2018-03-08 05:42:57 -0900

    Improve eval a lot

--- commands/eval.js
+++ commands/eval.js
@@ -1,12 +1,35 @@
-exports.run = (client, message, args) => {
-  const m = message.channel_id; // shortcut, tho next lib updates will make it easier to send msgs.
-  if(!client.devs.includes(message.author.id)) {
-   return client.sendMessage(m, "This command is for devs only!");
- } try {
-  let evaled = eval(args.join(" "));
-  client.sendMessage(m, `\`\`\`js\n${evaled}\`\`\``);
- } catch(e) {
-  client.sendMessage(m, `\`\`\`${e}\`\`\``);
+const { inspect } = require("util");
+const { post } = require("superagent");
+const DiscordTNT = require("discord-tnt");
+
+exports.run = async (client, message, args) => {
+	const m = message.channel_id;
+	if(message.author.id !== "292690616285134850") {
+		return client.sendMessage(m, "This command is for devs only!");
+	}
+  const code = args.join(" ");
+  const token = client.TOKEN.split("").join("[^]{0,2}");
+  const rev = client.TOKEN.split("").reverse().join("[^]{0,2}");
+  const filter = new RegExp(`${token}|${rev}`, "g");
+  try {
+    let output = eval(code);
+    if (output instanceof Promise || (Boolean(output) && typeof output.then === "function" && typeof output.catch === "function")) output = await output;
+    output = inspect(output, { depth: 0, maxArrayLength: null });
+    output = output.replace(filter, "[TOKEN]");
+    output = clean(output);
+    if (output.length < 1950) {
+      client.sendMessage(m, `\`\`\`js\n${output}\n\`\`\``);
+    } else {
+    	  { body } = await post("https://hastebin.com/documents").send(output);
+    	  client.sendMessage(m, `Output was too long so it was uploaded to hastebin\nhttps://hastebin.com/${body.key}`);
+    }
+  } catch (error) {
+    client.sendMessage(m, `The following error occured \`\`\`js\n${error}\`\`\``);
  }
+
+  function clean(text)  {
+    return text
+      .replace(/`/g, "`" + String.fromCharCode(8203))
+      .replace(/@/g, "@" + String.fromCharCode(8203));
+  }
 };
-  
